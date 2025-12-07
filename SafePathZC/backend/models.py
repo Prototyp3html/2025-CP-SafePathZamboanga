a2bfda4ecd6871be91af414b56c90fa305a5abe3
Prototyp3html <sevilla.jsivn@gmail.com>
2025-12-08 03:31:09 +0800

    fix crashed railway

--- SafePathZC/backend/models.py
+++ SafePathZC/backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, Text, create_engine, UniqueConstraint
+from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, Text, create_engine, UniqueConstraint, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
